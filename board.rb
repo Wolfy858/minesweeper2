21ac525e96b0e7297fcb780176f4748644f18e94
Elliot <Wolfy858@gmail.com>
2016-02-15 15:51:53 -0800

    added board class, additional methods for tile

---
+++ board.rb
@@ -0,0 +1,41 @@
+class Board
+  attr_reader :grid
+
+  def self.standard_grid
+    Array.new(9) { Array.new(9)}
+  end
+
+  def initialize(grid = Board.standard_grid)
+    @grid = grid
+    setup_tiles
+    populate_bombs(10)
+  end
+
+  def setup_tiles
+    @grid.each_with_index do |row, i|
+      row.each_with_index do |el, j|
+        self[i, j] = Tile.new(self, [i, j])
+      end
+    end
+  end
+
+  def populate_bombs(num)
+    bomb_count = 0
+    until bomb_count == num
+      current_tile = @grid.sample.sample
+      unless current_tile.bombed?
+        current_tile.bomb
+        bomb_count += 1
+      end
+    end
+  end
+
+  def [](row, col)
+    @grid[row][col]
+  end
+
+  def []=(row, col, value)
+    @grid[row][col]= value
+  end
+
+end
--- tile.rb
+++ tile.rb
@@ -1,3 +1,5 @@
+require './board'
+
 class Tile
   attr_reader :revealed
 
@@ -27,13 +29,17 @@ class Tile
     @bombed
   end
 
+  def bomb
+    @bombed = true
+  end
+
   def neighbors
-    adjacent_coords = deltas.map do |(dx, dy)|
-      [pos[0] + dx, pos[0] + dy]
+    adjacent_coords = DELTAS.map do |(dx, dy)|
+      [@pos[0] + dx, @pos[1] + dy]
     end.select do |row, col|
-      [row, col].all? {|coord| coord.between?(0, @board.length)}
+      [row, col].all? {|coord| coord.between?(0, @board.grid.length - 1)}
     end
-    adjacent_coords.map {|pos| @board[pos]}
+    adjacent_coords.map {|pos| @board[*pos]}
   end
 
   def neighbor_bomb_count
@@ -41,4 +47,18 @@ class Tile
     neighbors.each {|neighbor| count += 1 if neighbor.bombed? }
   end
 
+  def inspect
+    # {
+    #   :pos => @pos,
+    #   :bombed => @bombed,
+    #   :revealed => @revealed
+    # }
+    @bombed
+  end
+
 end
+
+b = Board.new
+p b
+# p b[0, 0].neighbors
+# p b[3, 3].neighbors
